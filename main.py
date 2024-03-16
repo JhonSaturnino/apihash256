34e690ead00a09512bf985eb6ca249bd33afd06a
JhonSaturnino <123093486+JhonSaturnino@users.noreply.github.com>
2024-03-16 19:53:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import hashlib
 from fastapi import FastAPI, HTTPException
+import requests
+import json
+import datetime
 
 app = FastAPI()
 
 @app.post("/processar_dados")
-def calcular_hash(nome: str, telefone: str, email: str):
+def calcular_hash(nome: str, telefone: str, email: str, nomeevento: str, tokenmeta: str, idpixel: str):
     if not (nome and telefone and email):
         raise HTTPException(status_code=400, detail='Erro: Nome, telefone e email são campos obrigatórios.')
     
@@ -13,11 +16,35 @@ def calcular_hash(nome: str, telefone: str, email: str):
     telefone_hash = hashlib.sha256(telefone.encode()).hexdigest()
     email_hash = hashlib.sha256(email.encode()).hexdigest()
 
-    # Retornar os hashes como resposta
-    resposta = {
-        'nome_hash': nome_hash,
-        'telefone_hash': telefone_hash,
-        'email_hash': email_hash
-    }
+    
+    url = f"https://graph.facebook.com/v18.0/{idpixel}/events?&access_token={tokenmeta}"
 
-    return resposta
+    agora = datetime.datetime.now()
+    milissegundos_desde_epoch = int(agora.timestamp() * 1000)
+   
+    dados = {
+    "data": [
+        {
+            "event_name": nomeevento,
+            "event_time": milissegundos_desde_epoch,
+            "action_source": "chat",
+            "user_data": {
+                "em": email_hash,
+                "ph": telefone_hash,
+                "fn": [
+                    nome_hash
+                ]
+            }
+        }
+    ]
+}
+
+
+    dados_json = json.dumps(dados)
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, data=dados_json, headers=headers)
+
+    if response.status_code == 200:
+       return response.status_code
+    else:
+        return response.status_code
